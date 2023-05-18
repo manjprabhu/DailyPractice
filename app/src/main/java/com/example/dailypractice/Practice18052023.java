b4315058eead5b7b0d9e0cce0af1ee561dbf4361
MPB <mp4learning@gmail.com>
2023-05-18 18:13:52 +0530

    Updated

--- app/src/main/java/com/example/dailypractice/Practice18052023.java
+++ app/src/main/java/com/example/dailypractice/Practice18052023.java
@@ -11,21 +11,38 @@ public class Practice18052023 {
 
     public void kthSmallestElement() {
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        int k =3;
-        int[] a = {10,4,7,99,12,45,5,6,1};
+        int k = 3;
+        int[] a = {10, 4, 7, 99, 12, 45, 5, 6, 1};
 
-        for(int i=0;i<k;i++) {
+        for (int i = 0; i < k; i++) {
             queue.add(a[i]);
         }
 
-        for(int i = k+1; i< a.length; i++) {
-            if(queue.peek() > a[i]) {
+        for (int i = k; i < a.length; i++) {
+            if (queue.peek() > a[i]) {
                 queue.poll();
                 queue.add(a[i]);
             }
         }
-        Log.d(TAG,k+"th smallest element is :"+ queue.peek());
+        Log.d(TAG, k + "th smallest element is :" + queue.peek());
 
     }
 
+
+    public void kthLargestElement() {
+        int k = 3;
+        int[] a = {10, 4, 7, 99, 12, 45, 5, 6, 1};
+        PriorityQueue<Integer> queue = new PriorityQueue();
+        for (int i = 0; i < k; i++) {
+            queue.add(a[i]);
+        }
+        for (int i = k; i < a.length; i++) {
+            if (queue.peek() < a[i]) {
+                queue.poll();
+                queue.add(a[i]);
+            }
+        }
+        Log.d(TAG, k + "th largest element is :" + queue.peek());
+
+    }
 }
